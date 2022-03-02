372371bf980bcbcbc3fd776dbb9c22ea6f8a4a5e
fethi-coder <fethibettahar@hotmail.fr>
2022-03-02 14:39:50 +0100

    animation

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { AnimationComponent } from './animation/animation.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AnimationComponent,
+
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
